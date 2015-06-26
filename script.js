143eea27725da2d116ca14ce65d48713adeeb2c8
Jake Wasdin <jake@wasdin.net>
2015-06-26 17:38:15 -0400

    now generating from predefined arrays

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
 }
 
+window.onload = function () {
 var first_names = ["Jake", "Joanne", "Bob", "Billy", "Scott", "Fred", "Anita", "Alma", "Janet", "Woodrow", "Elizabeth", "Charles", "George"];
-
-var last_names = ["Prolman", "Frank", "Shafer", "Lewis", "Whitefield", "Cathy", "Perkins", "Johnson", "Kraynak", "West", "Rice"];
+var last_names = ["Rice", "Smith", "Van Buren", "Washington", "Reese", "Watson", "Park", "Lee", "Cheng"];
+var settings = ["Beach", "Arctic tundra", "Waterpark", "Forest", "Theatre", "Mattress Factory", "Chicago alleyway", "Quiet Detroit suburb"];
+var motivations = ["Greed", "Revenge", "To be a doctor", "To find long last father", "To win the Olympics", "To become a dog trainer", "To be the world burrito eating champion"];
 
 var random_name = first_names.randomElement() + " " + last_names.randomElement();
+var random_setting = settings.randomElement();
+var random_motivation = motivations.randomElement();
 
-window.onload = function () {
-var change = document.getElementById("generated");
-change.innerHtml = random_name;
+document.getElementById("Name").innerHTML=random_name;
+document.getElementById("Setting").innerHTML=random_setting;
+document.getElementById("Motivation").innerHTML=random_motivation;
 
 }
 
-//alert(random_name);
 
